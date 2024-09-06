a6d9e715dda2a94db004a6c134fb636e59de644b
Sunkar Bauyrzhanuly <sunkar2710@gmail.com>
2024-09-06 18:20:23 +0500

    add method addOrder

--- src/main/java/Delivery.java
+++ src/main/java/Delivery.java
@@ -1,12 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.Scanner;
+import static java.nio.file.StandardOpenOption.*;
 
 public class Delivery {
+    static int customerNumber;
+
+    protected static void addOrder() {
+        Scanner sc = new Scanner(System.in);
+        Path waitingFile = Paths.get("./waitinglist.txt");
+        try (OutputStream out = new BufferedOutputStream(Files.newOutputStream(waitingFile, CREATE, APPEND))) {
+            out.write(String.format("Customer #%d:\n", ++customerNumber).getBytes());
+            String order;
+            int count = 1;
+            while (!Objects.equals(order = sc.nextLine(), "")) {
+                byte[] data = String.format("%d. %s\n", count, order).getBytes();
+                out.write(data);
+                count++;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     protected static void displayMenu() {
         Path file = Paths.get("./menu.txt");
         try (InputStream in = Files.newInputStream(file);
